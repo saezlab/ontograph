c49bdb9239f2adad67af794731754a7ef6d30e1b
Edwin Carreño <ecarrenolozano@gmail.com>
2025-07-18 13:30:34 +0200

    chores: comment all the look&feel (possible client interface) using line comments rather a big docstring. It removes errors when the linter runs.

--- ontograph/core.py
+++ ontograph/core.py
@@ -1,105 +1,71 @@
-from dataclasses import dataclass
-from pronto import Ontology
-
 # Import the logic to deal with OBO Foundry registry (Implementation)
-from ontology_registry import OBORegistryAdapter
-
 # Import the logic to download files (Implementation)
 from downloader import PoochDownloaderAdapter
+from ontology_registry import OBORegistryAdapter
 
 # Import the logic to load ontologies (Implementation)
-from ontology_loader import ProntoLoaderAdapter
 
 
-cache_dir = "../data/out"
+cache_dir = '../data/out'
 
 ontologies_registry = OBORegistryAdapter(cache_dir=cache_dir)
 
-downloader = PoochDownloaderAdapter(cache_dir=cache_dir, registry=ontologies_registry)
+downloader = PoochDownloaderAdapter(
+    cache_dir=cache_dir, registry=ontologies_registry
+)
 
 
-class Ontology:
-    def __init__(self):
-        pass
+# import ontograph as op
 
 
-class Registry:
-    def __init__(self):
-        pass
+# onto_registry = op.registry()
+
+# onto_registry.list_ontologies()
+
+# onto_registry.load_registry()
+
+# onto_registry.registry()
+
+# onto_registry.list_available_ontologies()
+
+# onto_registry.print_registry_schema_tree()
+
+# onto_registry.get_ontology_metadata()
+
+# onto_registry.get_download_url()
+
+# onto_registry.get_available_formats()
 
 
-class Client:
-    def __init__(self):
-        self.__registry  # This one needs to be instantiated Just one time (singleton?)
-        self.__ontology  # This one can be instantiated multiple times
+# # Similar to Pandas Dataframe Look & Feel
+# go = op.load(path: str, name_id: str, format: str = "obo")
 
-    # methods for manipulating the registry
-    def __initialize_registry():
-        pass
+# go.get_parents(term_id: str, depth: int = -1, include_self: bool = False)
 
-    # methods for manipulating the ontology
-    def __initialize_ontology():
-        pass
+# go.get_children(term_id: str, depth: int = -1, include_self: bool = False)
 
+# go.get_siblings(term_id)
 
-# 1.
-"""
+# go.get_roots()
 
-import ontograph as op
+# go.get_leaves()
 
+# go.find_path(source_id, target_id)
 
-onto_registry = op.registry() 
+# go.is_descendant(child_id, parent_id)
 
-onto_registry.list_ontologies()
+# go.get_depth(term_id)
 
-onto_registry.load_registry()
+# go.list_terms(prefix="GO:", include_obsolete=False)
 
-onto_registry.registry()
+# go.get_term_info(term_id)
 
-onto_registry.list_available_ontologies()
+# go.search_terms(query="metabolic process", exact=False)
 
-onto_registry.print_registry_schema_tree()
+# go.get_relationships(term_id)
 
-onto_registry.get_ontology_metadata()
+# go.get_terms_by_relation(terms="part_of")
 
-onto_registry.get_download_url()
+# go.get_common_ancestors(term_ids)
 
-onto_registry.get_available_formats()
-
-
-
-# Similar to Pandas Dataframe Look & Feel
-go = op.load(path: str, name_id: str, format: str = "obo")
-
-go.get_parents(term_id: str, depth: int = -1, include_self: bool = False)
-
-go.get_children(term_id: str, depth: int = -1, include_self: bool = False)
-
-go.get_siblings(term_id)
-
-go.get_roots()
-
-go.get_leaves()
-
-go.find_path(source_id, target_id)
-
-go.is_descendant(child_id, parent_id)
-
-go.get_depth(term_id)
-
-go.list_terms(prefix="GO:", include_obsolete=False)
-
-go.get_term_info(term_id)
-
-go.search_terms(query="metabolic process", exact=False)
-
-go.get_relationships(term_id)
-
-go.get_terms_by_relation(terms="part_of")
-
-go.get_common_ancestors(term_ids)
-
-go.term_exists(term_id)
-
-
-"""
+# go.term_exists(term_id)
